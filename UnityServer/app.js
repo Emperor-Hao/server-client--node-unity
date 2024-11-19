c6435b5cad65faa44ff8f5fb9d42e11826acb720
郑浩 <3077569207@qq.com>
2024-11-19 13:14:58 +0800

    node+unity前后端对接 unityWebRequest

---
+++ UnityServer/app.js
@@ -0,0 +1,36 @@
+'use strict';
+var express = require("express");
+var path = require("path");
+var app = express();
+const PORT = process.env.PORT  || 3000
+app.listen(PORT,(err) => {
+    if(err)
+    {
+       console.log("连接出错！");
+    }
+    console.log("连接正常","http://127.0.0.1:" + PORT);
+    } );
+
+    
+app.use("/",express.static(path.join(process.cwd(),"www_root")));
+
+app.get("/uploadData",function(req,res){
+    res.setHeader('Content-Type', 'text/plain');
+    const id = req.query.Id;
+    console.log(id);
+    res.send(`Hello Unity, you sent Id=${id}`);
+    res.send("hello unity",id);
+});
+
+
+var fs = require("fs");
+app.put("/UploadImgFile",function(req,res){
+    var fd = fs.openSync("./upload/unity2022.gif","w");
+    req.on("data",function(data){
+        fs.write(fd,data,0,data.length,function(){});
+    });
+    req.on("end",function(){
+        res.send("UploadSucess!");
+        fs.close(fd,function(){});
+    });
+});
